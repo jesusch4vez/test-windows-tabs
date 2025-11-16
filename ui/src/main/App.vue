9ee6b10693ba96039c01d6584b27ab67201b07c2
jesus <jesus.s.chavez01@gmail.com>
2025-11-16 15:50:26 -0500

    reorganizing

--- ui/src/main/App.vue
+++ ui/src/main/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import HelloWorld from '@/main/components/HelloWorld.vue'
-import TheWelcome from '@/main/components/TheWelcome.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./main/resources/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./resources/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
--- ui/src/main/main.ts
+++ ui/src/main/main.ts
@@ -1,4 +1,4 @@
-import '@/main/resources/assets/main.css'
+import './resources/assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
